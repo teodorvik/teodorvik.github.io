0bb4d6ebc9608c4b55387fcdc08b44c771a1c429
Teodor Vik <teovi698@student.liu.se>
2015-11-30 12:39:49 +0100

    Update rss.php

--- lab-rss/rss.php
+++ lab-rss/rss.php
@@ -14,11 +14,6 @@
 
  <?php  
 
-    $host ="tv-195283.mysql.binero.se";
-    $username = "195283_gm88388";
-    $password = "teo4ever";
-    $name = "195283-tv";
-
     $link = @mysql_connect($host, $username, $password) or die("Could not connect");
     mysql_select_db($name) or die("Could not select database");
     $returnstring ="";
